e173e8fc820dd92ac35744cc77b711fc073d4591
donsiddhart@gmail.com <donsiddhart@gmail.com>
2023-12-04 17:14:59 +0530

    conflict resolved: on line 7 , my code is  retained

--- src/demoClasses/GreetingDemo.java
+++ src/demoClasses/GreetingDemo.java
@@ -4,7 +4,7 @@ public class GreetingDemo {
 
 	public static void main(String[] args) 
 	{
-		System.out.println("Some other developer changed code");
+		System.out.println("changes made by me");
 		System.out.println("This is change done by other developer");
 	}
 
